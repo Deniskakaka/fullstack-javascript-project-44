b7109e51c3122080e99b2d61a1a7e8c907135821
Deniskakaka <krollik1000000@gmail.com>
2022-10-16 14:14:45 +0300

    fix eslint

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ export const review = (extension, startQuestion, count, name, right) => {
   if (count === 0) console.log(`${startQuestion}`);
   console.log(`Question: ${extension}`);
   const answear = readlineSync.question('Your answear:', 10);
-  return check(!parseInt(answear, 10) ? answear : +answear, count, name, right);
+  return check(!parseInt(answear) ? answear : +answear, count, name, right);
 };
 
 export const returnRandomNumber = (min = 1, max = 100) => {
